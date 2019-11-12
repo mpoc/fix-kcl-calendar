f5f9b95ab3e9a76aad50ed5fde82d83f39c3cf17
Marius <marius.pocev@gmail.com>
2019-11-12 23:51:00 +0000

    Add code

---
+++ src/main/java/me/mpoc/fixkclcalendar/Main.java
@@ -0,0 +1,55 @@
+package me.mpoc.fixkclcalendar;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.component.CalendarComponent;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// PUT YOUR CALENDAR SUBSCRIPTION LINK HERE:
+		InputStream is = new URL("").openStream();
+		CalendarBuilder builder = new CalendarBuilder();
+		Calendar calendar = builder.build(is);
+
+		for (CalendarComponent calendarComponent : calendar.getComponents()) {
+			for (Property property : calendarComponent.getProperties()) {
+				if (property.getName().equals("DESCRIPTION")) {
+					String[] descriptionTags = property.getValue().split("\n");
+
+					HashMap<String, String> splitDescriptionTags = new HashMap<>();
+
+					Arrays.stream(descriptionTags)
+						  .forEach(tag -> {
+							String[] split = tag.split(": ");
+							splitDescriptionTags.put(split[0], split[1]);
+						  });
+
+					String nameOfLecture = splitDescriptionTags.get("Description");
+					String typeOfLecture = splitDescriptionTags.get("Event type");
+
+					if (nameOfLecture != null) {
+						if (typeOfLecture != null) {
+							calendarComponent.getProperty(Property.SUMMARY).setValue(nameOfLecture + " " + typeOfLecture);
+						}
+						else {
+							calendarComponent.getProperty(Property.SUMMARY).setValue(nameOfLecture);
+						}
+					}
+				}
+			}
+		}
+
+		FileOutputStream fout = new FileOutputStream("Marius_adjusted.ics");
+		CalendarOutputter outputter = new CalendarOutputter(false);
+		outputter.output(calendar, fout);
+	}
+}
