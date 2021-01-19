bd02f290ed4e9c1c4f59f6cad57a4895f354a7b9
Marius <marius.pocev@gmail.com>
2021-01-19 13:13:41 +0200

    Add calendar parsing

--- convert.py
+++ convert.py
@@ -1,10 +1,9 @@
 import sys
 import requests
-
-
-
+from icalendar import Calendar
 
 url = sys.argv[1]
 calendar = requests.get(url).text
+ical = Calendar.from_ical(calendar)
 
-print(calendar)
+print(ical)
